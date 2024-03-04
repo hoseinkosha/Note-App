bc0897b61a47e291bc1af921138e5befa9dc0ed1
hosseinkosha <hosein.put24@gmail.com>
2024-03-04 12:18:53 +0330

    change style file

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,3 @@ import App from "./App.js";
 const root = document.getElementById("app");
 const app = new App(root);
 
-// view.updateNoteList(NotesAPI.getAllNotes());
-// view.updateActiveNote(NotesAPI.getAllNotes()[1]);
